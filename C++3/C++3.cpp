e1b6f157b7db49e0f9ed62a7a64185113648c859
Женя Босюк <bosuk21@gmail.com>
2019-06-06 17:56:49 +0300

    Добавьте файлы проекта.

---
+++ C++3/C++3.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char mas[12];
+	cout << "Enter 12 symbols: " << endl;
+	for (int i = 0; i < 12; i++)
+	{
+		cin >> mas[i];
+	}
+
+	for (int i = 0; i < 12; i++)
+	{
+		if (isdigit(mas[i]))
+			mas[i] = '!';
+	}
+	for (int i = 0; i < 12; i++)
+	{
+		cout << mas[i] << " ";
+	}
+	return 0;
+}
+
